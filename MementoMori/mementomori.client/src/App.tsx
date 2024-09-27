0523c78b7d0c5e49f36724e9a52756d08aa33d80
Karolis Petrovskis <karolis.petrovskis@mif.stud.vu.lt>
2024-09-27 10:50:32 +0300

    make fronted SPA

--- MementoMori/mementomori.client/src/App.tsx
+++ MementoMori/mementomori.client/src/App.tsx
@@ -1,10 +1,22 @@
-import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Broswer from './deckBrowser/Browser';
 import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-
+const client = new QueryClient();
 
 function App() {
-    return <span> Development will begin soon </span>;
+  return (
+    <QueryClientProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/browser" element={<Broswer />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
