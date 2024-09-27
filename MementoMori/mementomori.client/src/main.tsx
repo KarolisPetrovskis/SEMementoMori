6e3a1cb5dfd83049aac687430c54392057c4a6b9
Karolis Petrovskis <karolis.petrovskis@mif.stud.vu.lt>
2024-09-27 10:50:32 +0300

    make fronted SPA

--- MementoMori/mementomori.client/src/main.tsx
+++ MementoMori/mementomori.client/src/main.tsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import BasicCard from './MainFunctionality.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-        <BasicCard />
-  </StrictMode>,
-)
+    <App />
+  </StrictMode>
+);
