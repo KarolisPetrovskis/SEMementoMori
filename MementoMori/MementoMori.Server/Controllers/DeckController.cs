3494d64299f8030883be4756063577a11214cc2a
Paulius Navikas <paulius.navikas@mif.stud.vu.lt>
2024-10-01 01:12:07 +0300

    Clean up, added a simple Deck controller

--- MementoMori/MementoMori.Server/Controllers/DeckController.cs
+++ MementoMori/MementoMori.Server/Controllers/DeckController.cs
@@ -1,16 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MementoMori.Server.Controllers
 {
-    //[ApiController]
-    //[Route("[decks]")]
- //   public class DeckController: ControllerBase
-	//{
- //       //decks/deck => info apie deck
- //       [HttpGet(Name = "deck")]
- //       //public ?<?> Get() { 
- //       //    localhost:????/decks/id/deck
- //       //}
+    [ApiController]
+    [Route("[controller]")]
 
- //   }
+
+    public class DeckBrowserController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetDecks()
+        {
+            return Ok(TestDeck.Decks);
+
+        }
+
+        [HttpGet("{deckId}")]
+        public IActionResult GetDeck(Guid deckId) 
+        {
+            var deck = TestDeck.Decks.FirstOrDefaul(deck  => deck.Id == deckId);
+
+            if (deck == null)
+                return NotFound("Deck not found.");
+
+            return Ok(deck);
+        }
+        
+        //Create deck
+
+        //Get the next card of a deck by priority (don't know if it should be in the controller)
 }
