7c8d5487a250939ea99f5ec63c4c8ff81dac4462
Paulius Navikas <paulius.navikas@mif.stud.vu.lt>
2024-10-04 21:42:21 +0300

    preparing front end

--- MementoMori/MementoMori.Server/Controllers/DeckController.cs
+++ MementoMori/MementoMori.Server/Controllers/DeckController.cs
@@ -1,35 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace MementoMori.Server.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+    using System.Linq;
 
-
-    public class DeckBrowserController : ControllerBase
+    namespace MementoMori.Server.Controllers
     {
+        [ApiController]
+        [Route("[controller]")]
 
-        [HttpGet]
-        public IActionResult GetDecks()
+
+        public class DeckBrowserController : ControllerBase
         {
-            return Ok(TestDeck.Decks);
 
+            [HttpGet]
+            public IActionResult GetDecks()
+            {
+                return Ok(TestDeck.Decks);
+
+            }
+
+            [HttpGet("{deckId}/deck")]
+            public IActionResult GetDeck(Guid deckId)
+            {
+                var deck = TestDeck.Decks.FirstOrDefault(deck => deck.Id == deckId);
+
+                if (deck == null)
+                    return NotFound("Deck not found.");
+
+                return Ok(deck);
+            }
+
+            //Create deck
+
+            //Get the next card of a deck by priority (don't know if it should be in the controller)
         }
-
-        [HttpGet("{deckId}")]
-        public IActionResult GetDeck(Guid deckId) 
-        {
-            var deck = TestDeck.Decks.FirstOrDefaul(deck  => deck.Id == deckId);
-
-            if (deck == null)
-                return NotFound("Deck not found.");
-
-            return Ok(deck);
-        }
-        
-        //Create deck
-
-        //Get the next card of a deck by priority (don't know if it should be in the controller)
+    }
 }
