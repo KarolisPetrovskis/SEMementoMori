899e50963cea2e8bb00977779e27b8d339ade55d
NikasNikasNikas <nikas.jxd@gmail.com>
2024-10-14 17:41:47 +0300

    Adding of a service and joining card cration and reading to front-end output.
    * Added changes to the data input in cardCreator
    
    * Changed axios returned data name for CardCreator
    
    * Changed so the id is as a Guid and addressed any issues that arise from that changes.
    
    * Changed the usie of CardData.cs  in CardFileReader and controller to use Card.cs
    
    * Cleanup
    
    * Changed file that is shown in SpecificDeck.tsx
    
    * Joined the card creation for specific deck in deck browser.
    
    * Delete MementoMori/MementoMori.Server/CardFile/001.txt
    
    * Delete a .txt file.
    
    * Joined cardData reading from file with DeckFile card viewer.
    
    ---------
    
    Co-authored-by: Nikas Zabukas <nikas.zabukas@mif.stud.vu.lt>

--- MementoMori/MementoMori.Server/Controllers/CardFileController.cs
+++ MementoMori/MementoMori.Server/Controllers/CardFileController.cs
@@ -16,7 +16,7 @@ namespace MementoMori.Server.Controllers
 			_cardFileReader = cardFileReader;
 			string serverDirectory = Directory.GetCurrentDirectory();
 			// Assuming the file is always 001.txt if you want to display more files in a static way then you can do modifications inf GetFileContent
-			_filePath = Path.Combine(serverDirectory, "CardFile", "001.txt"); 
+			_filePath = Path.Combine(serverDirectory, "CardFile", "7c9e6679-7425-40de-944b-e07fc1f90ae7.txt"); 
 		}
 
 		[HttpGet("getFileContent")]
@@ -30,9 +30,9 @@ namespace MementoMori.Server.Controllers
 			try
 			{
                 // Do all the information packaging here
-				string[] fileContent = _cardFileReader.ExtractCards(_filePath);
-				// Return file content as an array of strings
-				return Ok(fileContent);
+				Card[] fileContent = _cardFileReader.ExtractCards(_filePath);
+                // Return file content as an array of CardData
+                return Ok(fileContent);
 			}
 			catch (Exception ex)
 			{
