de544a78e4353a6ca5d196628a766f7a29e88ba5
Karolis Petrovskis <karolis.petrovskis@mif.stud.vu.lt>
2024-10-14 16:40:10 +0300

    Added better for frontend for connecting the features together

--- MementoMori/MementoMori.Server/DTOS/DeckEditorDTO.cs
+++ MementoMori/MementoMori.Server/DTOS/DeckEditorDTO.cs
@@ -2,20 +2,20 @@ using MementoMori.Server.Models;
 
 namespace MementoMori.Server.DTOS
 {
-    public class DeckEditorDTO
-    {
-        public required Guid Id { get; set; }
+        public class DeckEditorDTO
+        {
+                public required Guid Id { get; set; }
 
-        public required bool isPublic { get; set; }
+                public required bool isPublic { get; set; }
 
-        public required string Title { get; set; }
+                public required string Title { get; set; }
 
-        public string? Description { get; set; }
+                public string? Description { get; set; }
 
-        public required long CardCount { get; set; }
+                public required long CardCount { get; set; }
 
-        public List<string>? Tags { get; set; }
+                public List<string>? Tags { get; set; }
 
-        public CardDTO[]? Cards { get; set; }
-    }
+                public CardDTO[]? Cards { get; set; }
+        }
 }
