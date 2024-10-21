d72170ff8963d13b680c84d09dba545fb6c27530
Karolis Petrovskis <karolis.petrovskis@mif.stud.vu.lt>
2024-10-21 16:51:57 +0300

    add basic db setup and initial migration, cards table will probably need to be split

---
+++ MementoMori/MementoMori.Server/Database/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace MementoMori.Server.Database
+{
+    public class AppDbContext : DbContext 
+    {
+        protected readonly IConfiguration Configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+        public DbSet<Deck> Decks { get; set; }
+    }
+
+}
