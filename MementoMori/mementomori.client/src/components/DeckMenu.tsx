f74ecce427103be5e63ccafbf5cf63e8fcbfb6eb
Danielius Vickacka <danieliusvickacka@gmail.com>
2024-10-21 18:07:48 +0300

    Home page changed layout, removed buttons, added deck list with temp data. WIP

--- MementoMori/mementomori.client/src/components/DeckMenu.tsx
+++ MementoMori/mementomori.client/src/components/DeckMenu.tsx
@@ -1,70 +1,80 @@
 import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import SettingsIcon from '@mui/icons-material/Settings';
 import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import StyleIcon from '@mui/icons-material/Style';
-
-const options = ['Deck 1', 'Deck 2', 'Deck 3'];
-
-const ITEM_HEIGHT = 48;
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
 
 export default function DeckMenu() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
   return (
-    <div>
-      <IconButton
-        aria-label="more"
-        id="long-button"
-        aria-controls={open ? 'long-menu' : undefined}
-        aria-expanded={open ? 'true' : undefined}
-        aria-haspopup="true"
-        onClick={handleClick}
-      >
-        <StyleIcon sx={{ color: 'white' }} />
-      </IconButton>
-      <Menu
-        id="long-menu"
-        MenuListProps={{
-          'aria-labelledby': 'long-button',
-        }}
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        slotProps={{
-          paper: {
-            sx: {
-              border: 1,
-              borderWidth: 2,
-              borderColor: '#D4A017',
-              bgcolor: '#6E6DB3',
-              color: '#ecffff',
-              borderRadius: '16px',
-            },
-            style: {
-              maxHeight: ITEM_HEIGHT * 4.5,
-              width: '20ch',
-            },
-          },
+    <div style={{ position: 'relative' }}>
+      <Typography
+        variant="h6"
+        sx={{
+          color: 'black',
+          position: 'absolute',
+          top: 15,
+          background: 'white',
+          padding: 2,
+          width: 268,
+          border: 1,
+          borderTopLeftRadius: '6px',
+          borderTopRightRadius: '6px',
+          borderBottomRightRadius: '0px',
+          borderBottomLeftRadius: '0px',
+          borderColor: '#D4A017',
+          borderWidth: 2,
         }}
       >
-        {options.map((option) => (
-          <MenuItem
-            key={option}
-            selected={option === 'Pyxis'}
-            onClick={handleClose}
-          >
-            {option}
-          </MenuItem>
+        Decks
+      </Typography>
+      <List
+        sx={{
+          position: 'absolute',
+          top: 80,
+          minWidth: 300,
+          maxWidth: 300,
+          maxHeight: 400,
+          overflowY: 'auto',
+          border: 1,
+          borderTopLeftRadius: '0px',
+          borderTopRightRadius: '0px',
+          borderBottomRightRadius: '6px',
+          borderBottomLeftRadius: '6px',
+          borderColor: '#D4A017',
+          borderWidth: 2,
+          bgcolor: 'white',
+        }}
+      >
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((value, index) => (
+          <React.Fragment key={value}>
+            <ListItem disableGutters>
+              <Button
+                variant="text"
+                sx={{ cursor: 'pointer' }}
+                onClick={() => {
+                  // Handle button click here
+                  console.log(`Line item ${value} clicked`);
+                }}
+              >
+                <ListItemText primary={`Line item ${value}`} />
+              </Button>
+              <IconButton
+                aria-label="settings"
+                sx={{ cursor: 'pointer' }}
+                style={{ marginLeft: 'auto' }}
+              >
+                <SettingsIcon />
+              </IconButton>
+            </ListItem>
+            {index !== 11 && <Divider />}{' '}
+            {/* Render Divider only if not the last item  TO BE CHANGED*/}
+          </React.Fragment>
         ))}
-      </Menu>
+      </List>
     </div>
   );
 }
