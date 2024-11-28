024a13f288129ba7e752d467dcf7e6c62ca61450
Nikas Zabukas <nikas.zabukas@mif.stud.vu.lt>
2024-11-28 20:47:24 +0200

    Testing unit test fix

--- MementoMori/MementoMori.Server/Database/AppDbContext.cs
+++ MementoMori/MementoMori.Server/Database/AppDbContext.cs
@@ -17,6 +17,8 @@ namespace MementoMori.Server.Database
                 .HasForeignKey(c => c.DeckId)
                 .OnDelete(DeleteBehavior.Cascade);
 
+            modelBuilder.Entity<User>().ToTable("Users");
+
             base.OnModelCreating(modelBuilder);
         }
         public void SecureUpdate<T, P>(P item, Guid changedBy) where T : P where P : DatabaseObject
